a70a0c00e0a863f006c1b7a29d439b0e3c773b3e
LAPTOP-4TJAVEAO\maria <cmariahangela@gmail.com>
2025-12-07 14:52:31 -0600

    style: clean up formatting and remove unnecessary comments in App component + make it validator ready

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,26 @@ import { Footer } from './components/Footer';
 import { Card } from './components/Card';
 
 function App() {
-    return (
-        <>
-            {/* Header */}
-            <Header title="Welcome to My Website!" message="Thanks for visiting my site." />
+  return (
+    <>
+      <Header title="Welcome to My Website!" message="Thanks for visiting my site." />
 
-            {/* Todo */}
-            <TodoList todos={[
-                { id: 1, text: "Finish reading a book before 2026", completed: false },
-                { id: 2, text: "Finish one LinkedIn Learning course", completed: false },
-                { id: 3, text: "Declutter bedroom closet", completed: true }
-            ]} />
+      <TodoList todos={[
+        { id: 1, text: "Finish reading a book before 2026", completed: false },
+        { id: 2, text: "Finish one LinkedIn Learning course", completed: false },
+        { id: 3, text: "Declutter bedroom closet", completed: false }
+      ]} />
 
-            {/* Card */}
-            <Card
-                title="My Card Title"
-                subtitle="My Card Subtitle"
-                content="This is the content of my card."
-                image="https://placecats.com/neo_2/300/200"
-            />
+      <Card
+        title="My Card Title"
+        subtitle="My Card Subtitle"
+        content="This is the content of my card."
+        image="https://placecats.com/neo_2/300/200"
+      />
 
-            {/* Footer */}
-            <Footer message="Contact me at contact@mywebsite.com" />
-        </>
-    );
+      <Footer message="Contact me at contact@mywebsite.com" />
+    </>
+  );
 }
 
 export default App;
